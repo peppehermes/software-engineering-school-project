cbcdf3d9129de16c85005ff20ce8d8fe9893b9f3
Sahar saadatmandi <sahar.saadatmandi@outlook.it>
2019-11-05 10:02:10 +0100

    first commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        <h2>This is the administration panel</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
