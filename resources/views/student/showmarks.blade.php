fa273075d47ac6d7e7b1916b382858e11272fd25
Sahar saadatmandi <sahar.saadatmandi@outlook.it>
2019-11-14 21:12:46 +0100

    class composition

--- resources/views/student/showmarks.blade.php
+++ resources/views/student/showmarks.blade.php
@@ -15,13 +15,13 @@
                         </div>
                         <div class="sparkline13-graph">
                             <div class="datatable-dashv1-list custom-datatable-overright">
-                                <div id="toolbar">
-                                    <select class="form-control dt-tb">
-                                        <option value="">Export Basic</option>
-                                        <option value="all">Export All</option>
-                                        <option value="selected">Export Selected</option>
-                                    </select>
-                                </div>
+{{--                                <div id="toolbar">--}}
+{{--                                    <select class="form-control dt-tb">--}}
+{{--                                        <option value="">Export Basic</option>--}}
+{{--                                        <option value="all">Export All</option>--}}
+{{--                                        <option value="selected">Export Selected</option>--}}
+{{--                                    </select>--}}
+{{--                                </div>--}}
                                 <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
                                        data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
                                     <thead>
