72c951b1b302053ed30024b0ae97dbaaa309c52a
Montynator14 <56630169+Montynator14@users.noreply.github.com>
2019-11-25 17:45:03 +0100

    Added constraints on story #11

--- resources/views/assignments/add.blade.php
+++ resources/views/assignments/add.blade.php
@@ -1,6 +1,69 @@
 @extends('layouts.layout')
 @section('content')
 
+    <script>
+
+
+function checkDate() {
+
+   var  extraday,d,m,d1,m1;
+
+    d  = document.getElementById('day').value;
+    m  = document.getElementById('month').value;
+    d1 = document.getElementById('dayd').value;
+    m1 = document.getElementById('monthd').value;
+
+
+
+    const today = new Date(),
+    dayweek=today.getDay(),
+    daymonth=today.getDate(),
+    month=today.getMonth()+1,
+    lastday= daymonth - dayweek + (dayweek == 0 ? -6:1);
+
+    if(lastday<=0){
+
+        if(month == 5 || month == 7 || month == 10 || month == 12)
+            extraday=30+lastday;
+
+        else if(month==3)
+            extraday=28+lastday;
+
+        else
+            extraday=31+lastday;
+
+    }
+
+        if((lastday <= d && d <= daymonth && m==month)
+          ||(lastday <= 0 && extraday <= d  && m==month-1)
+            || (lastday <= 0 && d <= daymonth && m==month))
+       {
+           if((m1 != m && d1 <= d) || (m1 == m && d1 > d)) {
+               document.getElementById('demo1-upload').action = "/assignment/storeassignment";
+               document.getElementById('demo1-upload').method = "post";
+           }
+
+           else {
+               alert("Wrong Deadline!"+d1+d+m1+m);
+               document.getElementById('dayd').focus();
+               document.getElementById('yeard').focus();
+               document.getElementById('monthd').focus();
+           }
+
+       }
+        else {
+        alert("Wrong lecture's date!");
+        document.getElementById('day').focus();
+        document.getElementById('year').focus();
+        document.getElementById('month').focus();
+       }
+
+}
+
+
+    </script>
+
+
     <!-- Single pro tab review Start-->
     <div class="single-pro-review-area mt-t-30 mg-b-15">
         <div class="container-fluid">
@@ -19,7 +82,7 @@
                                         <div class="review-content-section">
                                             <div id="dropzone1" class="pro-ad">
                                                 <form class="dropzone dropzone-custom needsclick add-professors"
-                                                      id="demo1-upload" enctype="multipart/form-data" action="/assignment/storeassignment" method="post" name="form">
+                                                      id="demo1-upload" enctype="multipart/form-data" onsubmit="checkDate()" name="form">
                                                     @csrf
                                                     <div class="row">
                                                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
@@ -42,8 +105,11 @@
 
                                                             <div class="form-group col-md-6">
                                                                 <label>Subject:</label>
-                                                                <input name="frm[subject]" type="text"
-                                                                       class="form-control" required>
+                                                                <select name="subject" class="form-control" required>
+                                                                @foreach($classes as $class)
+                                                                    <option value="{{$class->subject}}">{{$class->subject}}</option>
+                                                                @endforeach
+                                                                </select>
                                                             </div>
 
                                                             <div class="form-group col-md-6">
@@ -61,9 +127,7 @@
                                                                     <option value="none"  selected="" disabled="">
                                                                         Year
                                                                     </option>
-                                                                    @for($i=2019 ;  $i<2021 ; $i++)
-                                                                        <option value="{{$i}}">{{$i}}</option>
-                                                                    @endfor
+                                                                    <option value="{{date("Y")}}">{{date("Y")}}</option>
                                                                 </select>
                                                             </div>
 
@@ -74,42 +138,10 @@
                                                                     <option value="none" selected="" disabled="">
                                                                         Month
                                                                     </option>
-                                                                    <option value="1">
-                                                                        January
-                                                                    </option>
-                                                                    <option value="2">
-                                                                        February
-                                                                    </option>
-                                                                    <option value="3">
-                                                                        March
-                                                                    </option>
-                                                                    <option value="4">
-                                                                        April
-                                                                    </option>
-                                                                    <option value="5">
-                                                                        May
-                                                                    </option>
-                                                                    <option value="6">
-                                                                        June
-                                                                    </option>
-                                                                    <option value="7">
-                                                                        July
-                                                                    </option>
-                                                                    <option value="8">
-                                                                        August
-                                                                    </option>
-                                                                    <option value="9">
-                                                                        September
-                                                                    </option>
-                                                                    <option value="10">
-                                                                        October
-                                                                    </option>
-                                                                    <option value="11">
-                                                                        November
-                                                                    </option>
-                                                                    <option value="12">
-                                                                        December
-                                                                    </option>
+                                                                    <option value="<?php $date = new DateTime("last month"); echo $date->format('m');?>">
+                                                                        <?php $date = new DateTime("last month"); echo $date->format('F');?></option>
+
+                                                                    <option value="{{date("m")}}">{{date("F")}}</option>
                                                                 </select>
 
                                                             </div>
@@ -135,9 +167,7 @@
                                                                     <option value="none"  selected="" disabled="">
                                                                         Year
                                                                     </option>
-                                                                    @for($i=2019 ;  $i<2021 ; $i++)
-                                                                        <option value="{{$i}}">{{$i}}</option>
-                                                                    @endfor
+                                                                    <option value="{{date("Y")}}">{{date("Y")}}</option>
                                                                 </select>
                                                             </div>
 
@@ -148,42 +178,13 @@
                                                                     <option value="none" selected="" disabled="">
                                                                         Month
                                                                     </option>
-                                                                    <option value="1">
-                                                                        January
-                                                                    </option>
-                                                                    <option value="2">
-                                                                        February
-                                                                    </option>
-                                                                    <option value="3">
-                                                                        March
-                                                                    </option>
-                                                                    <option value="4">
-                                                                        April
-                                                                    </option>
-                                                                    <option value="5">
-                                                                        May
-                                                                    </option>
-                                                                    <option value="6">
-                                                                        June
-                                                                    </option>
-                                                                    <option value="7">
-                                                                        July
-                                                                    </option>
-                                                                    <option value="8">
-                                                                        August
-                                                                    </option>
-                                                                    <option value="9">
-                                                                        September
-                                                                    </option>
-                                                                    <option value="10">
-                                                                        October
-                                                                    </option>
-                                                                    <option value="11">
-                                                                        November
-                                                                    </option>
-                                                                    <option value="12">
-                                                                        December
-                                                                    </option>
+
+                                                                    <option value="{{date("m")}}">{{date("F")}}</option>
+
+                                                                    <option value="<?php $date = new DateTime("next month"); echo $date->format('m');?>">
+                                                                        <?php $date = new DateTime("next month"); echo $date->format('F');?></option>
+
+
                                                                 </select>
 
                                                             </div>
